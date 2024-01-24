1e07062408d1594e948debc2b04280f1b9ac7c06
inha <inhaii@naver.com>
2024-01-24 22:54:19 +0900

    fix: SignUpComponent 수정

--- src/components/app/signUp/SignUpComponent.js
+++ src/components/app/signUp/SignUpComponent.js
@@ -9,18 +9,18 @@ function SignUpComponent() {
             <div className="signup-info-section">
                 <div className="signup-info-id">
                     <p className="signup-info-text">아이디</p>
-                    <input className="signup-info-text-input" placeholder="아이디"></input>
+                    <input className="signup-info-text-input" placeholder="아이디" />
                 </div>
                 <div className="signup-info-password">
                     <p className="signup-info-text">비밀번호</p>
                     <div className="signup-info-check-password">
-                        <input placeholder="비밀번호"></input>
-                        <input placeholder="비밀번호 확인"></input>
+                        <input placeholder="비밀번호" />
+                        <input placeholder="비밀번호 확인" />
                     </div>
                 </div>
                 <div className="signup-info-email">
                     <p className="signup-info-text">이메일</p>
-                    <input className="signup-info-email-input" placeholder="이메일"></input>
+                    <input className="signup-info-email-input" placeholder="이메일" />
                 </div>
             </div>
             <button className="signup-button">가입하기</button>
