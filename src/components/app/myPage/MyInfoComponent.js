f80aa359f4a43406b3b30374e12b7d0e9608cbaa
inha <inhaii@naver.com>
2024-03-13 12:21:35 +0900

    Add: MyInfoComponent, ReviewComponent ui추가

--- src/components/app/myPage/MyInfoComponent.js
+++ src/components/app/myPage/MyInfoComponent.js
@@ -6,7 +6,11 @@ function MyPageComponent() {
         <div className="my-info-container">
             <div className="my-info-section">
                 <div className="my-info-section-left">
-                    <p>조인하</p>
+                    <p>
+                        조인하 님은
+                        <br />
+                        아이언 등급입니다.
+                    </p>
                 </div>
                 <div className="my-info-section-right">
                     <div>
@@ -36,6 +40,32 @@ function MyPageComponent() {
                         </p>
                         <p>쿠폰</p>
                         <p>0개</p>
+                    </div>
+                </div>
+            </div>
+            <div className="my-info-middle-section">
+                <div className="my-info-middle-left">
+                    <p className="my-info-middle-my-shopping">나의 쇼핑활동</p>
+                    <p className="my-info-middle-my-review">리뷰 관리</p>
+                </div>
+                <div className="my-info-middle-right">
+                    <div className="my-info-middle-right-top">
+                        <p className="my-info-middle-order">최근 주문</p>
+                        <p className="my-info-middle-all">전체 보기</p>
+                    </div>
+                    <div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>주문일자</th>
+                                    <th>이미지</th>
+                                    <th>상품정보</th>
+                                    <th>수량</th>
+                                    <th>주문번호</th>
+                                    <th>주문금액</th>
+                                </tr>
+                            </thead>
+                        </table>
                     </div>
                 </div>
             </div>
--- src/components/app/review/ReviewComponent.js
+++ src/components/app/review/ReviewComponent.js
@@ -2,7 +2,20 @@ import { useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import './ReviewComponent.css';
 function ReviewComponent() {
-    return <div>review 페이지</div>;
+    return (
+        <div className="review-container">
+            <div className="review-section">
+                <ul>
+                    <li>작성 가능한 리뷰 0</li>
+                    <li>내가 작성한 리뷰 0</li>
+                </ul>
+            </div>
+            <div className="review-list-section">
+                <p className="review-list-possible-review">현재 작성가능한 리뷰가 없습니다.</p>
+                <p className="review-list-delivery">배송이 완료된 상품만 목록에 노출됩니다.</p>
+            </div>
+        </div>
+    );
 }
 
 export default ReviewComponent;
